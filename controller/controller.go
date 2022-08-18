78e4a48bbae721610264193daec61ac407fc3969
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-18 16:40:39 +0700

    feat : implement controller

--- controller/controller.go
+++ controller/controller.go
@@ -1,1 +1,18 @@
 package controller
+
+import (
+	"azura-lab-intern/study-case-1/controller/routes"
+	"azura-lab-intern/study-case-1/repository"
+	"net/http"
+)
+
+func NewMux(categoryRepo *repository.CategoryRepository, productRepo *repository.ProductRepository) *http.ServeMux {
+	m := http.NewServeMux()
+
+	m.Handle("/api/v1/products", routes.GetAllProduct(productRepo))
+	m.Handle("/api/v1/product/by/id", routes.GetProductByID(productRepo))
+	m.Handle("/api/v1/product/by/category", routes.GetAllProductByCategory(productRepo))
+	m.Handle("/api/v1/categories", routes.GetAllCategory(categoryRepo))
+
+	return m
+}
