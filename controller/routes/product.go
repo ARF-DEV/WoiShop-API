676ac237e9ee97af8165a7143c42d5674890e42a
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-18 11:36:09 +0700

    feat : implement getallproduct repo and route

--- controller/routes/product.go
+++ controller/routes/product.go
@@ -1,1 +1,20 @@
 package routes
+
+import (
+	"azura-lab-intern/study-case-1/helpers"
+	"azura-lab-intern/study-case-1/repository"
+	"net/http"
+)
+
+func GetAllProduct(productRepo *repository.ProductRepository) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		result, err := productRepo.GetAllProduct()
+
+		if err != nil {
+			helpers.ErrorResponseJSON(w, err.Message, http.StatusInternalServerError)
+			return
+		}
+
+		helpers.SuccessResponseJSON(w, "Success getting all product", result)
+	})
+}
--- repository/product.go
+++ repository/product.go
@@ -1,1 +1,47 @@
 package repository
+
+import (
+	"azura-lab-intern/study-case-1/models"
+	"database/sql"
+	"log"
+
+	"github.com/lib/pq"
+)
+
+type ProductRepository struct {
+	db *sql.DB
+}
+
+func CreateProductRepostitory(db *sql.DB) *ProductRepository {
+	return &ProductRepository{
+		db: db,
+	}
+}
+
+func (pr *ProductRepository) GetAllProduct() ([]models.Product, *pq.Error) {
+	sqlStatement := `SELECT id, name, price FROM product`
+
+	rows, err := pr.db.Query(sqlStatement)
+
+	if err != nil {
+		err := err.(*pq.Error)
+		log.Println("error on get all product : ", err.Message)
+		return nil, err
+	}
+
+	var products []models.Product
+	for rows.Next() {
+		var p models.Product
+
+		err := rows.Scan(&p.ID, &p.Name, &p.Price)
+
+		if err != nil {
+			err := err.(*pq.Error)
+			log.Println("error on get all product : ", err.Message)
+			return nil, err
+		}
+
+	}
+
+	return products, nil
+}
