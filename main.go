be43b1d41889d6b23191a9a1b5c789b4397f59b0
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-18 16:41:39 +0700

    chore : implement entry point code main.go

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"azura-lab-intern/study-case-1/controller"
+	"azura-lab-intern/study-case-1/repository"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "123456"
+	dbname   = "study_case_1"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database Connected!")
+
+	productRepo := repository.CreateProductRepository(db)
+	categoryRepo := repository.CreateCategoryRepository(db)
+
+	mux := controller.NewMux(categoryRepo, productRepo)
+
+	log.Println("Listening in port 8000")
+	http.ListenAndServe(":8000", mux)
 }
