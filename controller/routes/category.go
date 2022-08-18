b5817f76fe83c0979760ee2ea5864b3328a75cbd
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-18 16:29:09 +0700

    chore : implement repository and route for category

--- controller/routes/category.go
+++ controller/routes/category.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"azura-lab-intern/study-case-1/helpers"
+	"azura-lab-intern/study-case-1/repository"
+	"net/http"
+)
+
+func GetAllCategory(categoryRepo *repository.CategoryRepository) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		results, err := categoryRepo.GetAllCategory()
+
+		if err != nil {
+			helpers.ErrorResponseJSON(w, err.Message, http.StatusInternalServerError)
+		}
+
+		helpers.SuccessResponseJSON(w, "Success getting all category", results)
+	})
+}
--- repository/category.go
+++ repository/category.go
@@ -1,1 +1,49 @@
 package repository
+
+import (
+	"azura-lab-intern/study-case-1/models"
+	"database/sql"
+	"log"
+
+	"github.com/lib/pq"
+)
+
+type CategoryRepository struct {
+	db *sql.DB
+}
+
+func CreateCategoryRepository(db *sql.DB) *CategoryRepository {
+	return &CategoryRepository{
+		db: db,
+	}
+}
+
+func (cr *CategoryRepository) GetAllCategory() ([]models.Category, *pq.Error) {
+
+	sqlStatement := `SELECT id, name FROM category`
+
+	rows, err := cr.db.Query(sqlStatement)
+
+	if err != nil {
+		err := err.(*pq.Error)
+		log.Println("error on get all product : ", err.Message)
+		return nil, err
+	}
+
+	var results []models.Category
+	for rows.Next() {
+		var c models.Category
+
+		err := rows.Scan(&c.ID, &c.Name)
+
+		if err != nil {
+			err := err.(*pq.Error)
+			log.Println("error on get all product : ", err.Message)
+			return nil, err
+		}
+
+		results = append(results, c)
+	}
+
+	return results, nil
+}
