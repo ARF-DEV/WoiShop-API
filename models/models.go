0b0d9dad3f5e5b5ecca8532763c0e76123fb8d07
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-18 09:39:55 +0700

    feat(models/models.go) : Defined all database models

--- models/models.go
+++ models/models.go
@@ -1,1 +1,12 @@
 package models
+
+type Product struct {
+	ID    int    `db:"id"`
+	Name  string `db:"name"`
+	Price int    `db:"price"`
+}
+
+type Category struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+}
