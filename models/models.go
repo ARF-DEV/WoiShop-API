a357b29b610d2b2db93b13d1685a4b7e0ee73330
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-18 10:17:37 +0700

    refact(models/models.go) : add categoryID fifield

--- models/models.go
+++ models/models.go
@@ -1,9 +1,10 @@
 package models
 
 type Product struct {
-	ID    int    `db:"id"`
-	Name  string `db:"name"`
-	Price int    `db:"price"`
+	ID         int    `db:"id"`
+	CategoryID int    `db:"category_id"`
+	Name       string `db:"name"`
+	Price      int    `db:"price"`
 }
 
 type Category struct {
