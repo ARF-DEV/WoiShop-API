897768910957a8eeb85c9ebe82af0229e767040b
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-19 16:06:30 +0700

    refact : change Description type to sql.NullString

--- models/models.go
+++ models/models.go
@@ -1,11 +1,13 @@
 package models
 
+import "database/sql"
+
 type Product struct {
-	ID          int    `db:"id" json:"id"`
-	CategoryID  int    `db:"category_id" json:"category_id,omitempty"`
-	Name        string `db:"name" json:"name"`
-	Price       int    `db:"price" json:"price"`
-	Description string `db:"description" json:"description,omitempty"`
+	ID          int            `db:"id" json:"id"`
+	CategoryID  int            `db:"category_id" json:"category_id,omitempty"`
+	Name        string         `db:"name" json:"name"`
+	Price       int            `db:"price" json:"price"`
+	Description sql.NullString `db:"description" json:"description,omitempty"`
 }
 
 type Category struct {
