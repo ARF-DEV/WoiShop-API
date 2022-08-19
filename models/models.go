41cda8afb8ea02bd36c126a0b1e466d8712e1ca3
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-19 15:46:26 +0700

    refact : add descrition field on Product struct

--- models/models.go
+++ models/models.go
@@ -1,10 +1,11 @@
 package models
 
 type Product struct {
-	ID         int    `db:"id" json:"id"`
-	CategoryID int    `db:"category_id" json:"category_id,omitempty"`
-	Name       string `db:"name" json:"name"`
-	Price      int    `db:"price" json:"price"`
+	ID          int    `db:"id" json:"id"`
+	CategoryID  int    `db:"category_id" json:"category_id,omitempty"`
+	Name        string `db:"name" json:"name"`
+	Price       int    `db:"price" json:"price"`
+	Description string `db:"description" json:"description,omitempty"`
 }
 
 type Category struct {
