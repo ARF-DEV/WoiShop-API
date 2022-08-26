9fda8fd64fa8ab5fdb87e034cb080d4aea268820
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-26 23:30:07 +0700

    refact : login can be access using POST method

--- controller/controller.go
+++ controller/controller.go
@@ -14,6 +14,6 @@ func NewRouter(categoryRepo *repository.CategoryRepository, productRepo *reposit
 	r.Get("/api/v1/products/{id}", routes.GetProductByID(productRepo))
 	r.Get("/api/v1/products", routes.GetAllProduct(productRepo))
 	r.Get("/api/v1/categories", routes.GetAllCategory(categoryRepo))
-	r.Get("/api/v1/login", routes.Login(userRepo))
+	r.Post("/api/v1/login", routes.Login(userRepo))
 	return r
 }
