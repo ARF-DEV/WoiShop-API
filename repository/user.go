4c031b55682ba9148e56ee327943089d161be34d
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-26 23:12:28 +0700

    chore : create get user by email repository

--- repository/user.go
+++ repository/user.go
@@ -31,13 +31,32 @@ func (u *UserRepository) InsertUser(user models.User) error {
 
 func (u *UserRepository) GetUserByNoTelp(noTelp string) (*models.User, error) {
 	sqlStatement := `
-	SELECT id, full_name, phone_num, referal_code FROM user_data WHERE phone_num = ?
+	SELECT id, full_name, phone_num, email, referal_code FROM user_data WHERE phone_num = ?
 	`
 
 	row := u.db.QueryRow(sqlStatement, noTelp)
 
 	var user models.User
-	err := row.Scan(&user.ID, &user.Name, &user.NoTelp, &user.ReferalCode)
+	err := row.Scan(&user.ID, &user.Name, &user.NoTelp, &user.Email, &user.ReferalCode)
+
+	if err != nil {
+		log.Println("Error on Scan User By Telp: ", err.Error())
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (u *UserRepository) GetUserByEmail(email string) (*models.User, error) {
+
+	sqlStatement := `
+	SELECT id, full_name, phone_num, email, referal_code FROM user_data WHERE email = ?
+	`
+
+	row := u.db.QueryRow(sqlStatement, email)
+
+	var user models.User
+	err := row.Scan(&user.ID, &user.Name, &user.NoTelp, &user.Email, &user.ReferalCode)
 
 	if err != nil {
 		log.Println("Error on Scan User By Telp: ", err.Error())
