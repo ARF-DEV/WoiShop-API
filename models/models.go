fb9b0aba056b48a06c65c11045f3bdc4945ffa09
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-27 22:39:51 +0700

    refact : change type from sql.NullString to null.String

--- models/models.go
+++ models/models.go
@@ -1,14 +1,16 @@
 package models
 
-import "database/sql"
+import (
+	"gopkg.in/guregu/null.v3"
+)
 
 type Product struct {
-	ID          int            `db:"id" json:"id"`
-	CategoryID  int            `db:"category_id" json:"category_id,omitempty"`
-	Name        string         `db:"name" json:"name"`
-	Price       int            `db:"price" json:"price"`
-	Description sql.NullString `db:"description" json:"description,omitempty"`
-	ImageLink   sql.NullString `db:"image_link" json:"image_link"`
+	ID          int         `db:"id" json:"id"`
+	CategoryID  int         `db:"category_id" json:"category_id,omitempty"`
+	Name        string      `db:"name" json:"name"`
+	Price       int         `db:"price" json:"price"`
+	Description null.String `db:"description" json:"description,omitempty"`
+	ImageLink   null.String `db:"image_link" json:"image_link"`
 }
 
 type Category struct {
@@ -17,9 +19,9 @@ type Category struct {
 }
 
 type User struct {
-	ID          int            `db:"id" json:"id"`
-	Name        string         `db:"full_name" json:"full_name"`
-	NoTelp      string         `db:"phone_num" json:"phone_num"`
-	Email       string         `db:"email" json:"email"`
-	ReferalCode sql.NullString `db:"referal_code" json:"referal_code"`
+	ID          int      `db:"id" json:"id"`
+	Name        string   `db:"full_name" json:"full_name"`
+	NoTelp      string   `db:"phone_num" json:"phone_num"`
+	Email       string   `db:"email" json:"email"`
+	ReferalCode null.Int `db:"referal_code" json:"referal_code"`
 }
