f2bffe210bf5c6ac626f57040e9710ed9de4b809
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-09-03 07:17:36 +0700

    refact : move db config code to config package

---
+++ config/database.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func ConfigDatabase() *sql.DB {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database Connected!")
+	return db
+}
--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
+	"azura-lab-intern/study-case-1/config"
 	"azura-lab-intern/study-case-1/controller"
 	"azura-lab-intern/study-case-1/helpers"
 	"azura-lab-intern/study-case-1/repository"
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -22,13 +20,6 @@ var (
 )
 
 func main() {
-
-	err := godotenv.Load()
-
-	if err != nil {
-		panic(err.Error())
-	}
-
 	GoogleAuthConfig = &oauth2.Config{
 		RedirectURL:  "http://localhost:8000/api/v1/callback",
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
@@ -38,27 +29,7 @@ func main() {
 	}
 
 	OAuthStateString, _ = helpers.RandomString(10)
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
-
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Database Connected!")
-
+	db := config.ConfigDatabase()
 	helpers.InitDB(db)
 	productRepo := repository.CreateProductRepository(db)
 	categoryRepo := repository.CreateCategoryRepository(db)
