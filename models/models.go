6dadb5255e7103d089c4ebd79fafbe908e6faf15
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-09-06 20:04:28 +0700

    chore : add new fields to old models and add new models

--- models/models.go
+++ models/models.go
@@ -6,8 +6,10 @@ import (
 
 type Product struct {
 	ID          int         `db:"id" json:"id"`
+	StoreID     int         `db:"store_id" json:"store_id"`
 	CategoryID  int         `db:"category_id" json:"category_id,omitempty"`
 	Name        string      `db:"name" json:"name"`
+	Stock       int         `db:"stock" json:"stock"`
 	Price       int         `db:"price" json:"price"`
 	Description null.String `db:"description" json:"description,omitempty"`
 	ImageLink   null.String `db:"image_link" json:"image_link"`
@@ -24,4 +26,28 @@ type User struct {
 	NoTelp      string   `db:"phone_num" json:"phone_num"`
 	Email       string   `db:"email" json:"email"`
 	ReferalCode null.Int `db:"referal_code" json:"referal_code"`
+}
+
+type Order struct {
+	ID        int `db:"id" json:"id"`
+	CartID    int `db:"cart_id" json:"cart_id,omitempty"`
+	ProductID int `db:"product_id" json:"product_id,omitempty"`
+	Amount    int `db:"amount" json:"amount"`
+}
+
+type Cart struct {
+	ID               int         `db:"id" json:"id"`
+	UserID           int         `db:"user_id" json:"user_id"`
+	DeliveryMethodID int         `db:"delivery_method_id" json:"delivery_method_id"`
+	Note             null.String `db:"note" json:"note"`
+}
+
+type Store struct {
+	ID   int `db:"id" json:"id"`
+	Name int `db:"name" json:"name"`
+}
+
+type DeliveryMethod struct {
+	ID   int `db:"id" json:"id"`
+	Name int `db:"name" json:"name"`
 }
