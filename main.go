166c565ee80d9dc0dc268b31d75e946b5acda581
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-09-07 22:48:24 +0700

    chore : add some scopes to google oauth config

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"azura-lab-intern/study-case-1/controller"
 	"azura-lab-intern/study-case-1/helpers"
 	"azura-lab-intern/study-case-1/repository"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -20,13 +22,22 @@ var (
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		panic(err.Error())
+	}
 	GoogleAuthConfig = &oauth2.Config{
 		RedirectURL:  "http://localhost:8000/api/v1/callback",
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-		Endpoint:     google.Endpoint,
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.profile",
+			"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint: google.Endpoint,
 	}
+
+	fmt.Println(os.Getenv("GOOGLE_CLIENT_ID"))
 
 	OAuthStateString, _ = helpers.RandomString(10)
 	db := config.ConfigDatabase()
