f4ed91f7f89ce8ca04ba77ca6d108c179a48fd7e
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-09-07 22:57:08 +0700

    chore : middleware auth create user now when email its not registered yet

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,9 +2,13 @@ package middleware
 
 import (
 	"azura-lab-intern/study-case-1/helpers"
+	"azura-lab-intern/study-case-1/models"
+	"azura-lab-intern/study-case-1/repository"
 	"context"
+	"database/sql"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -26,7 +30,7 @@ func Method(method string, next http.Handler) http.Handler {
 	})
 }
 
-func Authorization(googleOAuthConfig *oauth2.Config) func(next http.Handler) http.Handler {
+func Authorization(googleOAuthConfig *oauth2.Config, userRepo *repository.UserRepository) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			bearer := r.Header.Get("Authorization")
@@ -61,8 +65,27 @@ func Authorization(googleOAuthConfig *oauth2.Config) func(next http.Handler) htt
 					return
 				}
 
-				log.Println(string(content))
-				ctx := context.WithValue(r.Context(), "user_oauth", content)
+				var content_map map[string]interface{}
+				json.Unmarshal(content, &content_map)
+				var user *models.User
+				user, err = userRepo.GetUserByEmail(content_map["email"].(string))
+
+				if err != nil {
+					if errors.Is(err, sql.ErrNoRows) {
+						newUser := models.User{
+							Name:  content_map["name"].(string),
+							Email: content_map["email"].(string),
+						}
+
+						user, _ = userRepo.InsertUser(newUser)
+					} else {
+						log.Println("Error while getting user : ", err.Error())
+						helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
+						return
+					}
+				}
+
+				ctx := context.WithValue(r.Context(), "user_data", user)
 				r = r.WithContext(ctx)
 
 			} else {
