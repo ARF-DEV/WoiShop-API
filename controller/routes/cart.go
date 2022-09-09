1e59a70e0626778cda39344aa8f3ebfda5e1bc8e
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-09-09 15:22:06 +0700

    feat : implement get cart by user ID (params) and Create cart

--- controller/routes/cart.go
+++ controller/routes/cart.go
@@ -12,7 +12,6 @@ import (
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
-	"gopkg.in/guregu/null.v3"
 )
 
 type OrderResponseSchema struct {
@@ -32,6 +31,66 @@ func GetCartByUserID(cartRepo *repository.CartRepository,
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		id := chi.URLParam(r, "id")
+		if len(id) < 1 {
+			log.Println("Error product by id : id query not found")
+			helpers.ErrorResponseJSON(w, "id query required", http.StatusBadRequest)
+			return
+		}
+		id_int, err := strconv.Atoi(id)
+		cart, err := cartRepo.GetCartByUserID(id_int)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				helpers.ErrorResponseJSON(w, "Cart is not found, please create the cart first", http.StatusInternalServerError)
+				return
+			}
+			log.Println("Error While getting Cart By ID : ", err.Error())
+			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		orders, err := orderRepo.GetOrderByCartID(cart.ID)
+
+		if err != nil {
+			log.Println("Error While getting Orders By Cart ID : ", err.Error())
+			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		var orderList []OrderResponseSchema = []OrderResponseSchema{}
+		for _, order := range orders {
+			var or OrderResponseSchema
+			product, err := productRepo.GetProductByID(order.ProductID)
+
+			if err != nil {
+				log.Println("Error While getting products : ", err.Error())
+				helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+
+			or = OrderResponseSchema{
+				Order:        order,
+				ProductName:  product.Name,
+				ProductPrice: product.Price,
+			}
+
+			orderList = append(orderList, or)
+		}
+
+		res := CartResponseSchema{
+			Cart:   *cart,
+			Orders: orderList,
+		}
+
+		helpers.SuccessResponseJSON(w, "Success Getting Cart By ID", res)
+
+	})
+}
+func GetCartByUserToken(cartRepo *repository.CartRepository,
+	orderRepo *repository.OrderRepository,
+	productRepo *repository.ProductRepository) http.HandlerFunc {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 		userData, ok := r.Context().Value("user_data").(models.User)
 		if !ok {
 			log.Println("userData not found")
@@ -40,18 +99,14 @@ func GetCartByUserID(cartRepo *repository.CartRepository,
 		}
 
 		cart, err := cartRepo.GetCartByUserID(userData.ID)
-		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				helpers.ErrorResponseJSON(w, "Cart is not found, please create the cart first", http.StatusInternalServerError)
+				return
+			}
 			log.Println("Error While getting Cart By ID : ", err.Error())
 			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
 			return
-		}
-
-		if errors.Is(err, sql.ErrNoRows) {
-			cart, _ = cartRepo.AddCart(models.Cart{
-				UserID:           userData.ID,
-				DeliveryMethodID: 1,
-				Note:             null.NewString("", false),
-			})
 		}
 
 		orders, err := orderRepo.GetOrderByCartID(cart.ID)
@@ -147,19 +202,59 @@ func GetAllCart(cartRepo *repository.CartRepository,
 	})
 }
 
+func CreateUserCart(cartRepo *repository.CartRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, _ := r.Context().Value("user_data").(models.User)
+		newCart, err := cartRepo.AddCart(models.Cart{
+			UserID:           user.ID,
+			DeliveryMethodID: 1,
+		})
+
+		if err != nil {
+			log.Println("Error while creating cart: ", err.Error())
+			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		helpers.SuccessResponseJSON(w, "Success Creating Cart", newCart)
+	}
+}
+
+func CreateCart(cartRepo *repository.CartRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var cartBody models.Cart
+
+		defer r.Body.Close()
+		err := json.NewDecoder(r.Body).Decode(&cartBody)
+
+		if err != nil {
+			log.Println("Error Decoding json : ", err.Error())
+			helpers.ErrorResponseJSON(w, "Json Body Is Invalid", http.StatusBadRequest)
+			return
+		}
+		createdCart, err := cartRepo.AddCart(cartBody)
+		if err != nil {
+			log.Println("Error while creating cart: ", err.Error())
+			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		helpers.SuccessResponseJSON(w, "Success Creating Cart", createdCart)
+	}
+}
 func DeleteCartByID(cartRepo *repository.CartRepository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id := chi.URLParam(r, "id")
 		if len(id) < 1 {
-			log.Println("Error product by id : id query not found")
-			helpers.ErrorResponseJSON(w, "id query required", http.StatusBadRequest)
+			log.Println("Error product by id : id params not found")
+			helpers.ErrorResponseJSON(w, "id params required", http.StatusBadRequest)
 			return
 		}
 		id_int, err := strconv.Atoi(id)
 
 		if err != nil {
 			log.Println("Error product by id : ", err.Error())
-			helpers.ErrorResponseJSON(w, "Invalid id query", http.StatusBadRequest)
+			helpers.ErrorResponseJSON(w, "Invalid id params", http.StatusBadRequest)
 			return
 		}
 
@@ -192,18 +287,15 @@ func AddOrderToCart(cartRepo *repository.CartRepository, orderRepo *repository.O
 
 		_, err = cartRepo.GetCartByID(OrderBody.CartID)
 
-		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+
+				helpers.ErrorResponseJSON(w, "Cart is not found, please create the cart first", http.StatusInternalServerError)
+				return
+			}
 			log.Println("Error while searching cart: ", err.Error())
 			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
 			return
-		}
-
-		if errors.Is(err, sql.ErrNoRows) {
-			user, _ := r.Context().Value("user_data").(models.User)
-			cartRepo.AddCart(models.Cart{
-				UserID:           user.ID,
-				DeliveryMethodID: 1,
-			})
 		}
 
 		newOrder, err := orderRepo.CreateOrder(OrderBody)
@@ -238,62 +330,5 @@ func UpdateCart(cartRepo *repository.CartRepository) http.HandlerFunc {
 		}
 
 		helpers.SuccessResponseJSON(w, "Success Updating cart", updatedCart)
-	}
-}
-
-func UpdateOrder(orderRepo *repository.OrderRepository) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var orderBody models.Order
-
-		defer r.Body.Close()
-		err := json.NewDecoder(r.Body).Decode(&orderBody)
-
-		if err != nil {
-			log.Println("Error Decoding json : ", err.Error())
-			helpers.ErrorResponseJSON(w, "Json Body Is Invalid", http.StatusBadRequest)
-			return
-		}
-		log.Println(orderBody)
-		log.Println("HELLO")
-		updatedOrder, err := orderRepo.ChangeOrderAmount(orderBody.ID, orderBody.Amount)
-		if err != nil {
-			log.Println("Error while updating order: ", err.Error())
-			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-
-		helpers.SuccessResponseJSON(w, "Success Updating cart", updatedOrder)
-	}
-}
-
-func DeleteOrderByID(orderRepo *repository.OrderRepository) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		id := chi.URLParam(r, "id")
-		if len(id) < 1 {
-			log.Println("Error product by id : id query not found")
-			helpers.ErrorResponseJSON(w, "id query required", http.StatusBadRequest)
-			return
-		}
-		id_int, err := strconv.Atoi(id)
-
-		if err != nil {
-			log.Println("Error product by id : ", err.Error())
-			helpers.ErrorResponseJSON(w, "Invalid id query", http.StatusBadRequest)
-			return
-		}
-
-		deletedOrder, err := orderRepo.DeleteOrderByID(id_int)
-
-		if err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				helpers.ErrorResponseJSON(w, "Order Not Found", http.StatusOK)
-				return
-			}
-			log.Println("Error when deleting order : ", err.Error())
-			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-
-		helpers.SuccessResponseJSON(w, "Success to delete order", deletedOrder)
 	}
 }
