cbae47db93301a932ddd721eac7996f92a1e0e29
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-09-09 13:31:36 +0700

    refact : change user_id values in seeder

--- seeder/cart.go
+++ seeder/cart.go
@@ -19,7 +19,7 @@ func MigrateCart(db *sql.DB) {
 		);
 		insert into cart (user_id, delivery_method_id, note) values (2, 1, 'Occup of bus injured in collision w hv veh in traf, sequela');
 		insert into cart (user_id, delivery_method_id, note) values (7, 5, 'Calcific tendinitis of unspecified shoulder');
-		insert into cart (user_id, delivery_method_id, note) values (3, 4, 'Displ commnt fx shaft of rad, l arm, 7thN');
+		insert into cart (user_id, delivery_method_id, note) values (4, 4, 'Displ commnt fx shaft of rad, l arm, 7thN');
 		insert into cart (user_id, delivery_method_id, note) values (3, 5, 'Nondisp fx of head of unsp rad, 7thQ');
 		insert into cart (user_id, delivery_method_id, note) values (11, 3, 'Toxic effect of smoke, undetermined, subsequent encounter');
 	`
