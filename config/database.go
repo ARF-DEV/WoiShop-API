4d835cd8d82b0fe6d2508e44de2989ee1700a110
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-09-19 14:26:04 +0700

    chore : try to deploy to heroku

--- config/database.go
+++ config/database.go
@@ -17,7 +17,7 @@ func ConfigDatabase() *sql.DB {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=enable",
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
 		host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
